94c4fa0446151cf5968d1d14af3c1d4817455623
Charlie Sahlman <csahlman@gmail.com>
2013-10-02 17:19:53 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EricMap::Application.config.secret_key_base = '7d5e02448e9e62040890e7f98818b8d52c1fee355b91cd239f0fdf0cc8a209b0da221c02fbae62f100908ba41ddf2b22bd50b8c57d117aaab558dea28db891a1'
